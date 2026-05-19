5e2a1539e9b329a6ba39e9e35764a63d770c0967
Denis89 <filippinidenis@gmail.com>
2026-05-19 21:27:10 +0200

    correzione esercizio

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {       // Route per la home page,
 foreach ($menu as $item) {
     $slug = Str::lower($item);              // Str è una classe di Laravel che contiene metodi per manipolare le stringhe, in questo caso strtolower() per trasformare la stringa in minuscolo
     Route::get('/' . $slug, function () { 
-        return view('home');
+        return view('home');                // restituisce sempre home.blade.php, ma $slug potrebbe restituire pagine con i nomi dei link
     })->name((string) $slug);               // (string) è un cast per convertire variabile in stringa (in questo caso $slug lo è già, ma è buona pratica per evitare errori)
 }
 
@@ -38,5 +38,6 @@ Route::get('/comic/{id}', function ($id) {  // {id} è un parametro dinamico che
     if (!isset($cards[$id])) {              // se l'id non esiste nell'array dei comics, restituisce un errore 404
         abort(404);
     }
-    return view('comic', ['comic' => $cards[$id], 'id' => $id]); // restituisce la vista comic.blade.php, passando i dati del comic e l'id alla pagina
+    $comic = $cards[$id];
+    return view('comic', compact('comic')); // restituisce la vista comic.blade.php, passando i dati del comic
 })->name('comic');
